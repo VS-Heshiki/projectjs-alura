fbf9479c4e9ea8edc4dabfda927ef877322c09ee
Victor Heshiki <victor_heshiki@hotmail.com>
2021-07-22 11:34:25 -0300

    addPaciente.js Correção Request

--- js/addPaciente.js
+++ js/addPaciente.js
@@ -1,22 +1,20 @@
-const botaoAdd = document.querySelector('#addPacientes');
-  botaoAdd.addEventListener('click', function(){
-    
-    var xhr = new XMLHttpRequest();
+const botaoAdd = document.querySelector("#addPacientes");
+botaoAdd.addEventListener("click", function () {
+  var xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'http://api-pacientes.herokuapp.com/pacientes');
+  xhr.open("GET", "http://api-pacientes.herokuapp.com/paciedsntes");
 
-    if(xhr.status === 200){
-      xhr.addEventListener('load', function(){
-        const request = xhr.responseText;
-        const pacientes = JSON.parse(request)
-  
-        pacientes.forEach(function(paciente){
-          addPacienteTabela(paciente);
-        })
+  xhr.addEventListener("load", function () {
+    if (xhr.status === 200) {
+      const request = xhr.responseText;
+      const pacientes = JSON.parse(request);
+
+      pacientes.forEach(function (paciente) {
+        addPacienteTabela(paciente);
       });
     } else {
       alert("Invalid Request!");
     }
-
-    xhr.send();
-  })
+  });
+  xhr.send();
+});
